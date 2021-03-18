dc6c144f7377a2905d49cc2eda560ee8eab14d52
Gabi Lapadat <lapadatrobert123@gmail.com>
2021-03-19 00:28:16 +0200

    Implemented stomp service functions for the public chat.

---
+++ src/services/stompService.js
@@ -0,0 +1,43 @@
+import * as SockJS from 'sockjs-client'
+import * as Stomp  from 'stompjs'
+
+let stompClient = null;
+
+export const connect = (username, onMessageReceived) => {
+
+    const socket = new SockJS('http://localhost:8080/heyyo')
+    stompClient = Stomp.over(socket)
+
+    stompClient.connect({ headers: {'Content-Type': 'text/plain', }}, 
+                        () => onConnected(username, onMessageReceived),     
+                        () => onError())
+}
+
+const onConnected = (username, onMessageReceived) => {
+
+    stompClient.subscribe('topic/public', onMessageReceived)
+
+    stompClient.send('app/chat.join', 
+                    {},
+                    JSON.stringify({sender: username, type: 'CONNECT', time: Date.now().toString()}))
+}
+
+const onError = () => {
+
+
+}
+
+export const send = (sender, messageContent) => {
+
+    if (messageContent && stompClient) {
+
+        const message = {
+            sender: sender,
+            content: messageContent,
+            type: 'CHAT',
+            time: Date.now().toString()
+        }
+
+        stompClient.send('/app/chat.send', {}, JSON.stringify(chatMessage))
+    }
+}
